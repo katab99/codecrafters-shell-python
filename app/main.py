68e8f321ebe0d72cfacbd800255457d7cbb9f973
Kata Bujdosó <kata@Katas-MacBook-Air.local>
2026-06-24 13:12:31 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,49 @@
 import sys
 
-
-def get_command(user_input):
-    return user_input.split()[0]
+COMMANDS = {}
 
 
-def run_echo(user_input):
-    return user_input[5:]
+def command(name: str):
+    def decorator(func):
+        COMMANDS[name] = {"action": func}
+        return func
+
+    return decorator
+
+
+@command("exit")
+def do_exit():
+    sys.exit()
+
+
+@command("echo")
+def do_echo(*args):
+    print(*args)
+
+
+@command("type")
+def do_type(*args):
+    cmd_name = args[0]
+
+    if cmd_name in COMMANDS:
+        print(f"{cmd_name} is a shell builtin")
+    else:
+        print(f"{cmd_name}: not found")
 
 
 def main():
     while True:
-        sys.stdout.write("$ ")
-        user_input = input()
-        command = get_command(user_input)
+        user_input = input("$ ").strip()
 
-        if command == "exit":
-            break
-        if command == "echo":
-            print(run_echo(user_input))
+        tokens = user_input.split()
+        command = tokens[0].lower()
+        args = tokens[1:]
+
+        # if command == "exit":
+        #     break
+
+        if command in COMMANDS:
+            COMMANDS[command]["action"](*args)
         else:
             print(f"{command}: command not found")
 
