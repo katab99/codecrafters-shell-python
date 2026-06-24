fb2fe2304fd2a984607a41cee393199ad21cc7fe
Kata Bujdosó <kata@Katas-MacBook-Air.local>
2026-06-24 10:35:11 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
 import sys
 
 
+def get_command(user_input):
+    return user_input.split()[0]
+
+
+def run_echo(user_input):
+    return user_input[5:]
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input()
+        command = get_command(user_input)
 
         if command == "exit":
             break
-
-        print(f"{command}: command not found")
+        if command == "echo":
+            print(run_echo(user_input))
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
